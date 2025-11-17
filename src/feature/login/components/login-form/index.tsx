83589a66c33522528cba4fe2df8200bf748d8ada
mohammad-dehdar <dehdardevelop@gmail.com>
2025-11-17 10:29:13 +0330

    fix login ui

--- src/feature/login/components/login-form/index.tsx
+++ src/feature/login/components/login-form/index.tsx
@@ -8,10 +8,10 @@ export const LoginForm = () => {
 
     return (
         <div className="relative z-10 h-svh flex w-full items-center justify-center">
-            <Card className="w-full max-w-md border border-neutral-40 dark:border-neutral-80 rounded-md shadow-md bg-neutral-10/80 dark:bg-neutral-90/70 backdrop-blur-md px-3 pb-1">
+            <Card className="w-full max-w-md border border-secondary-40 dark:border-secondary-80 rounded-md shadow-md bg-neutral-10/80 dark:bg-neutral-90/70 backdrop-blur-md px-3 pb-1">
                 <div className="space-y-8">
                     <header className="space-y-2 text-center">
-                        <p className="text-xs w-fit  mx-auto px-4 rounded-b-sm py-0.5 uppercase tracking-[0.35em] dark:bg-neutral-90/80 bg-neutral-60/80 text-neutral-10 border border-t-0 border-neutral-40 dark:border-neutral-80 dark:text-neutral-40">Etmify- HR</p>
+                        <p className="text-xs w-fit  mx-auto px-4 rounded-b-sm py-0.5 uppercase tracking-[0.35em] dark:bg-neutral-90/80 bg-neutral-60/80 text-neutral-10 border border-t-0 border-secondary-40 dark:border-secondary-80  dark:text-neutral-40">Etmify- HR</p>
                         <div className="space-y-2 mt-4">
                             <h1 className="text-2xl font-semibold text-neutral-90 dark:text-neutral-10">ورود به پروفایل کاربری</h1>
                             <p className="text-sm text-neutral-70 dark:text-neutral-40">برای دسترسی به داشبورد، اطلاعات خود را وارد کنید.</p>
@@ -21,7 +21,9 @@ export const LoginForm = () => {
                     <form onSubmit={handleSubmit} className="space-y-5">
                         <TextInput
                             type="email"
+                            variant="outline"
                             placeholder="ایمیل"
+                            color="secondary"
                             value={formData.email}
                             onChange={handleChange("email")}
                             state={errors.email ? "error" : undefined}
@@ -30,6 +32,8 @@ export const LoginForm = () => {
                         />
                         <TextInput
                             type="password"
+                            variant="outline"
+                            color="secondary"
                             placeholder="رمز عبور"
                             value={formData.password}
                             onChange={handleChange("password")}
